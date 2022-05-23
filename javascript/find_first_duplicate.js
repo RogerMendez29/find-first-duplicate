8dfda936ffba0bff1a05e80b4b9da7d7a3738bc4
Roger Mendez <roger61087@gmail.com>
2022-05-23 17:41:51 -0400

    done

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,17 +1,24 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  array = [];
+
+
+
+
+  for (i = 0; i < arr.length; i++) {
+    current = arr[i];
+
+    if (array.includes(current)) {
+      console.log(array)
+      return current;
+    }
+    array.push(current);
+  }
+
+  return -1;
+
 }
 
-if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 3");
-  console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
-
-  console.log("");
-
-  console.log("Expecting: -1");
-  console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
-}
+console.log(findFirstDuplicate([2, 1, 3, 3, 2]));
 
 module.exports = findFirstDuplicate;
 
